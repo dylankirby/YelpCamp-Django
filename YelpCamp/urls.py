f3f1a11d1817c2ed1d654d8198c4fc5692381452
dylankirby <dylankirby@outlook.com>
2017-09-13 16:15:29 -0700

    folder and file setup for users

--- YelpCamp/urls.py
+++ YelpCamp/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.HomePage.as_view(), name='home')
+    url(r'^$', views.HomePage.as_view(), name='home'),
+    url(r'^accounts/', include('accounts.urls')),
 ]
