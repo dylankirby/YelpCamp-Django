9eb00df8aaaeef231e63b88857644076223cf829
dylankirby <dylankirby@outlook.com>
2017-09-13 22:51:32 -0700

    fix logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,6 @@ app_name = 'accounts'
 
 urlpatterns = [
 	url(r'^login/$', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
-	url(r'^login/$', auth_views.LogoutView.as_view(), name="logout"),
+	url(r'^logout/$', auth_views.LogoutView.as_view(), name="logout"),
 	url(r'^signup/$', views.SignUp.as_view(), name="signup"),
 ]
