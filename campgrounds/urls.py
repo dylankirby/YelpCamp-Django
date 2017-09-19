332251f72aefd7f6480503a0cbe24998c99b8a6f
dylankirby <dylankirby@outlook.com>
2017-09-19 08:16:54 -0700

    campground detail view setup

--- campgrounds/urls.py
+++ campgrounds/urls.py
@@ -8,4 +8,5 @@ app_name = 'campgrounds'
 urlpatterns = [
 	url(r'^$', views.CampgroundList.as_view(), name='all'),
 	url(r'^new/$', views.CreateCampground.as_view(), name='create'),
+	url(r'^(?P<pk>\d+)$', views.CampgroundDetail.as_view(), name='details')
 ]
