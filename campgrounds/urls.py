b93544a558366d46ae9f3c8bacf75b566ed62785
dylankirby <dylankirby@outlook.com>
2017-09-21 12:50:28 -0700

    campground update/delete view

--- campgrounds/urls.py
+++ campgrounds/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
 	url(r'^$', views.CampgroundList.as_view(), name='all'),
 	url(r'^new/$', views.CreateCampground.as_view(), name='create'),
 	url(r'^(?P<pk>\d+)$', views.CampgroundDetail.as_view(), name='details'),
-	url(r'^delete/(?P<pk>\d+)$', views.CampgroundDetail.as_view(), name='details'),
+	url(r'^update/(?P<pk>\d+)/$', views.UpdateCampground.as_view(), name='update'),
+	url(r'^delete/(?P<pk>\d+)/$', views.DeleteCampground.as_view(), name='delete'),
 ]
--- campgrounds/views.py
+++ campgrounds/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.http import Http404
 from django.views import generic
 from campgrounds.models import Campground
 # Create your views here.
 
-class CreateCampground(LoginRequiredMixin,generic.CreateView):
+class CreateCampground(LoginRequiredMixin, generic.CreateView):
 	fields = ('name', 'image', 'description')
 	model = Campground
 
@@ -16,7 +17,22 @@ class CreateCampground(LoginRequiredMixin,generic.CreateView):
 class CampgroundDetail(generic.DetailView):
 	model = Campground
 
+# add in get_query_set to filter by location
 class CampgroundList(generic.ListView):
 	model = Campground
+
+class UpdateCampground(LoginRequiredMixin, generic.UpdateView):
+	model = Campground
+	fields = ('name', 'image', 'description')
+
+	template_name_suffix = '_update_form'
+
+class DeleteCampground(LoginRequiredMixin, generic.DeleteView):
+	model = Campground
+	success_url = reverse_lazy('campgrounds:all')
+	
+	def delete(self,*args, **kwargs):
+		return super().delete(*args, **kwargs)
+
 
 
