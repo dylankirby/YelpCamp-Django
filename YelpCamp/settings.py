d489b4b8a446bfc20c7640896192e87d68cd1ef2
dylankirby <dylankirby@outlook.com>
2017-10-13 15:10:40 -0700

    password hashing update

--- YelpCamp/settings.py
+++ YelpCamp/settings.py
@@ -84,6 +84,14 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
